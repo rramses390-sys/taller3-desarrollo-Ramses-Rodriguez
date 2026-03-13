792a3de727a7201a4e25dad72bbcf835e5709ec3
rramses390 <rramses390@gmail.com>
2026-03-12 20:04:01 -0500

    primera parte del codigo de lista de compra inteligente

---
+++ SmartShopping.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SmartShopping {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    ArrayList<String> shoppinglist = new ArrayList<String>();
+
+  }
+}
